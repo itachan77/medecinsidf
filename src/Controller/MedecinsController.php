03f8ea2b9502718df2aaa3767c3f4738bfe7e1ba
itachan77 <itachan77@orange.fr>
2021-10-09 23:31:27 +0200

    enr

--- src/Controller/MedecinsController.php
+++ src/Controller/MedecinsController.php
@@ -2,19 +2,55 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MedecinsController extends AbstractController
 {
     /**
-     * @Route("/medecins", name="medecins")
+     * @Route("/regions", name="region")
      */
-    public function index(): Response
+    public function regions(RegionsRepository $regionsRepository): Response
     {
-        return $this->render('medecins/index.html.twig', [
-            'controller_name' => 'MedecinsController',
-        ]);
+
+            /***********DATA REGION */
+        $regions=$regionsRepository->findAll();
+        $region=[];
+        if ($regions)
+        {
+            foreach($regions as $val) {
+                $region[]=[
+                    'id'=>$val->getId(),
+                    'code'=>$val->getCode(),
+                    'name'=>$val->getName(),];
+            }
+        }
+        $dataRegion = json_encode($region);
+        
+        //JsonResponse retourne une valeur en Json. ex: {"id":11,"nomDiscipline":"Anglais","isDone":false}
+        $response = new JsonResponse();
+
+        return $response->setData($infoApis); //(le paramètre comme $discipline correspond toujours à un tableau)
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
