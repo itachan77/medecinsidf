2328c620abca1fb91f934a035f4b7cb78b6e6a73
itachan77 <itachan77@orange.fr>
2021-10-11 14:55:16 +0200

    api

--- src/Controller/MedecinsController.php
+++ src/Controller/MedecinsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\CpvilleRepository;
 use App\Repository\RegionsRepository;
 use App\Repository\DepartmentsRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -95,6 +96,22 @@ class MedecinsController extends AbstractController
         $response = new JsonResponse();
 
         return $response->setData($ville); //(le paramètre comme $discipline correspond toujours à un tableau)
+  
+    }  
+
+    /**
+     * @Route("/medecinsIdf", name="medecinsIdf")
+     */
+    public function medecinsIdf(Request $request): Response
+    {
+
+        $api=$request(
+            'GET', "https://data.iledefrance.fr/api/records/1.0/search/?dataset=annuaire-et-localisation-des-professionnels-de-sante&q=&rows=9999&facet=civilite&facet=exercice_particulier&facet=nature_exercice&facet=convention&facet=sesam_vitale&facet=types_actes&facet=codes_ccam&facet=nom_epci&facet=nom_dep&facet=nom_reg&facet=nom_com&facet=libelle_profession",
+        );
+        //JsonResponse retourne une valeur en Json. ex: {"id":11,"nomDiscipline":"Anglais","isDone":false}
+        $response = new JsonResponse();
+
+        return $response->setData($api); //(le paramètre comme $discipline correspond toujours à un tableau)
   
     }  
     
